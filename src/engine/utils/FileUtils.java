8383f4cfb6a03b4093ba6c176e2e7421736150b2
Ar-personal <reid0alex@gmail.com>
2021-02-17 13:14:24 +0000

    Base engine with view and projection matrices, mesh and material compatability

---
+++ src/engine/utils/FileUtils.java
@@ -0,0 +1,23 @@
+package engine.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class FileUtils {
+    public static String loadAsString(String path) {
+        StringBuilder result = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(FileUtils.class.getResourceAsStream(path)))) {
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                result.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Couldn't find the file at " + path);
+        }
+
+        return result.toString();
+    }
+}
